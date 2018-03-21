25d6b16ff98fc18dd98aeec76bb34ba98073388c
Jesse Lucas <jesse@jesselucas.com>
2018-03-21 14:57:22 -0400

    Creating symlink.sh as a setup script to symlink my dotfiles to the appropriate destinations

---
+++ symlink.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Check to see if we are in the dotfiles git directory
+GITORIGIN=$( git config --get remote.origin.url )
+if [ "$GITORIGIN" != "git@github.com:jesselucas/dotfiles.git" ]; then
+  echo "Script must be ran from within jesselucas/dotfiles git repo"
+  exit 1
+fi
+
+# Set dotfiles directory to pwd
+DOTFILES=$( pwd )
+src[0]=$DOTFILES
+src[1]=$DOTFILES/amp
+
+# Create a map like structure of destinations and files
+# index is used as key to associate both dest and files
+dest[0]=$HOME
+dest[1]="$HOME/Library/Application Support/amp"
+
+# Set files as values
+files0=( .bashrc .tmux.conf .vimrc .git-prompt-colors.sh )
+files1=( config.yml )
+
+# Loop over each destination and use i as a key
+# and the value is files$i array
+for i in "${!dest[@]}"; do
+  d=${dest[$i]}
+  s=${src[$i]}
+  vArray=files$i[@]
+
+  # Loop through all files in each values array
+  for f in "${!vArray}"; do
+    srcPath=$s/$f
+    destPath=$d/$f
+
+    # Test if destPath doesn't exist and srchPath exist
+    if [ ! -e "${destPath}" ] && [ -e "${srcPath}" ]; then
+      ln -s "${srcPath}" "${destPath}"
+      echo "Symlinking: $srcPath to $destPath"
+    else
+      echo "Skipping: $destPath"
+    fi
+  done
+done
