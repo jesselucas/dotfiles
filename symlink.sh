406b123d041665fb8892cff35009095d4638ce6e
Jesse Lucas <jesse@jesselucas.com>
2018-04-16 22:35:11 -0400

    Add shell syntax highlighting
    Using http://www.sublimetext.com/docs/3/syntax.html to allow
    syntax highlighting for shell scripts

--- symlink.sh
+++ symlink.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 # Check to see if we are in the dotfiles git directory
 GITORIGIN=$( git config --get remote.origin.url )
 if [ "$GITORIGIN" != "git@github.com:jesselucas/dotfiles.git" ]; then
@@ -8,20 +8,25 @@ fi
 
 # Set dotfiles directory to pwd
 DOTFILES=$( pwd )
+
+# Create src directories
 src[0]=$DOTFILES
 src[1]=$DOTFILES/amp
 src[2]=$DOTFILES/amp/themes
+src[3]=$DOTFILES/amp/syntaxes
 
 # Create a map like structure of destinations and files
 # index is used as key to associate both dest and files
 dest[0]=$HOME
 dest[1]="$HOME/Library/Application Support/amp"
 dest[2]="$HOME/Library/Application Support/amp/themes"
+dest[3]="$HOME/Library/Application Support/amp/syntaxes"
 
 # Set files as values
 files0=( .bashrc .tmux.conf .vimrc .git-prompt-colors.sh )
 files1=( config.yml )
-files2=( Tomorrow-Night-Eighties.tmTheme ) 
+files2=( Tomorrow-Night-Eighties.tmTheme )
+files3=( Shell-Unix-Generic.sublime-syntax )
 
 # Loop over each destination and use i as a key
 # and the value is files$i array
