9c32020315fd150bfcdd61daf71133762208856f
Jesse Lucas <jesse@jesselucas.com>
2018-05-19 16:34:38 -0400

    Update symlink to work with ksh and bash
    Replace array expansion with eval of echo

--- symlink.sh
+++ symlink.sh
@@ -1,13 +1,19 @@
-#!/bin/bash
+#!/bin/sh -
+#
+#
+
+# Turn off Strict Bourne shell mode.
+set +o sh
+
 # Check to see if we are in the dotfiles git directory
-GITORIGIN=$( git config --get remote.origin.url )
+GITORIGIN=`git config --get remote.origin.url`
 if [ "$GITORIGIN" != "git@github.com:jesselucas/dotfiles.git" ]; then
-  echo "Script must be ran from within jesselucas/dotfiles git repo"
-  exit 1
+	echo "Script must be ran from within jesselucas/dotfiles git repo"
+	exit 1
 fi
 
 # Set dotfiles directory to pwd
-DOTFILES=$( pwd )
+DOTFILES=`pwd`
 
 # Create src directories
 src[0]=$DOTFILES
@@ -25,30 +31,37 @@ dest[3]="$HOME/Library/Application Support/amp/syntaxes"
 dest[4]="$HOME/.config/alacritty"
 
 # Set files as values
-files0=( .bashrc .tmux.conf .vimrc .git-prompt-colors.sh )
-files1=( config.yml )
-files2=( Tomorrow-Night-Eighties.tmTheme )
-files3=( Shell-Unix-Generic.sublime-syntax )
-files4=( alacritty.yml )
+files0[0]=".bashrc" 
+files0[1]=".tmux.conf" 
+files0[2]=".vimrc" 
+files0[3]=".git-prompt-colors.sh"
+files1[0]="config.yml"
+files2[0]="Tomorrow-Night-Eighties.tmTheme"
+files3[0]="Shell-Unix-Generic.sublime-syntax"
+files4[0]="alacritty.yml"
 
 # Loop over each destination and use i as a key
 # and the value is files$i array
-for i in "${!dest[@]}"; do
-  d=${dest[$i]}
-  s=${src[$i]}
-  vArray=files$i[@]
+i=0
+for _ in "${dest[*]}"; do
+	d=${dest[$i]}
+	s=${src[$i]}
+	vArray=files$i
+  	
+	# Loop through all files in each values array
+	vArray=$(eval echo \${files$i[*]})
+	for f in $vArray; do
+		srcPath=$s/$f
+		destPath=$d/$f
 
-  # Loop through all files in each values array
-  for f in "${!vArray}"; do
-    srcPath=$s/$f
-    destPath=$d/$f
-
-    # Test if destPath doesn't exist and srchPath exist
-    if [ ! -e "${destPath}" ] && [ -e "${srcPath}" ]; then
-      ln -s "${srcPath}" "${destPath}"
-      echo "Symlinking: $srcPath to $destPath"
-    else
-      echo "Skipping: $destPath"
-    fi
-  done
+		# Test if destPath doesn't exist and srchPath exist
+		if [ ! -e "${destPath}" ] && [ -e "${srcPath}" ]; then
+			ln -s "${srcPath}" "${destPath}"
+			echo "Symlinking: $srcPath to $destPath"
+		else
+			echo "Skipping: $destPath"
+		fi
+	done
+	# Increment i.
+	i=i+1	
 done
