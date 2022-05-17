9e1bc99ba1c0dd18be327c8c4dcbdc481fa447e2
Jesse Lucas <jesse@jesselucas.com>
2022-05-16 21:35:11 -0400

    clangd

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,20 @@
 vim.g.mapleader = ","
 vim.g.localleader = "\\"
 
-
 require('vars')
 require('opts')
 require('keys')
 require('plugins')
+
 require('nvim-tree-setup')
+require('formatter-setup')
 
 require'hop'.setup()
-require'lspconfig'.clangd.setup{}
+require'lspconfig'.clangd.setup {
+    cmd = {
+        'clangd', '--clang-tidy', '--background-index', '--std=c11',
+        '--suggest-missing-includes', '--all-scopes-completion',
+        '--header-insertion=iwyu', '--completion-style=detailed',
+        '--pch-storage=memory'
+    },
+}
