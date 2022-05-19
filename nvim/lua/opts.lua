939c9722eb97345956d01b429b95ad1c7a76af7a
Jesse Lucas <jesse@jesselucas.com>
2022-05-19 19:51:35 -0400

    used tabs

--- nvim/lua/opts.lua
+++ nvim/lua/opts.lua
@@ -20,7 +20,6 @@ opt.incsearch = true -- bool: Use incremental search
 opt.hlsearch = false -- bool: Highlight search matches
 
 -- [[ Whitespace ]]
-opt.expandtab = true -- bool: Use spaces instead of tabs
 opt.shiftwidth = 4 -- num:  Size of an indent
 opt.softtabstop = 4 -- num:  Number of spaces tabs count for in insert mode
 opt.tabstop = 4 -- num:  Number of spaces tabs count for
