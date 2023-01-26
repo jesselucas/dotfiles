ce51d5f275dce41e6caff6233ce6bfca2626bf84
Jesse Lucas <jesse@jesselucas.com>
2023-01-26 10:42:09 -0500

    add nvim auto format

--- nvim/lua/formatter-setup.lua
+++ nvim/lua/formatter-setup.lua
@@ -5,7 +5,18 @@ require("formatter").setup({
 		c = { require("formatter.filetypes.c").clangformat },
 		lua = { require("formatter.filetypes.lua").stylua },
 		rust = { require('formatter.filetypes.rust').rustfmt },
-		zig = { require('formatter.filetypes.zig').zigfmt },
+--		zig = { require('formatter.filetypes.zig').zigfmt },
 		go = { require('formatter.filetypes.go').gofmt },
 	},
 })
+
+vim.api.nvim_exec(
+       [[
+    augroup FormatAutogroup
+           autocmd!
+               autocmd BufWritePost * FormatWrite
+               augroup END
+       ]],
+       true
+)
+
